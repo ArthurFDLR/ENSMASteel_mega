33ef92c666aedb774739e94fef146f62934b8a53
Benoit <benoit.varillon@gmail.com>
2019-04-26 12:52:49 +0200

    specification du barillet des pompes et de l'elevateur (barillet.h)

--- Mega/mega/barillet.h
+++ Mega/mega/barillet.h
@@ -1,1 +1,46 @@
+class barillet
+{
+	public:
+	
+	barillet();
+	
+	int get_pos();	// obtenir position actuelle
+	void set_pos(int pos);	// aller à la position souhaité
+	void turn(int nb); // tourner de nb case (+ sens trigo, - sens horraire)
+	
+	
+	private:
+	
+	int m_pos;
+};
 
+class pompe
+{
+	public:
+	
+	pompe();
+	
+	void take(); // prendre un pallet
+	void release(); // lacher le pallet
+	
+	
+	private:
+	
+	bool taken;
+}
+
+enum state_elev {UP,DOWN};
+
+class elevator
+{
+	public:
+	
+	elevator();
+	
+	void up(); // faire monter les ventouses
+	void down(); // faire descendre les ventouses
+	
+	
+	private:
+	state_elev state;
+}
